b470752bc1a9ef7c8ea9b537dd37b80af817e640
Albert Dahlin <info@albertdahlin.com>
2021-08-02 18:09:14 +0200

    Add example for stream

--- example/src/Stream.elm
+++ example/src/Stream.elm
@@ -1,8 +1,12 @@
 module Stream exposing (..)
 
-{-| Example of reading a stream'
+{-| Example of reading a stream.
+
+Read a CSV file line by line and convert each row to a JSON object.
+
 -}
 
+import Json.Encode as Encode
 import Posix.IO as IO exposing (IO)
 import Posix.IO.File as File
 import Posix.IO.Stream as Stream exposing (Stream)
@@ -12,12 +16,39 @@ import Posix.IO.Stream as Stream exposing (Stream)
 -}
 program : IO.Process -> IO String ()
 program process =
-    File.openReadStream { bufferSize = 10 } "elm.json"
+    File.openReadStream { bufferSize = 10 } "test.csv"
         |> IO.andThen
-            (\src ->
-                src
-                    |> Stream.pipeTo Stream.utf8Decode
-                    |> Stream.forEach IO.print
+            (\testCsvStream ->
+                let
+                    rowStream =
+                        testCsvStream
+                            |> Stream.pipeTo Stream.utf8Decode
+                            |> Stream.pipeTo Stream.line
+                in
+                Stream.read rowStream
+                    |> IO.andThen
+                        (\maybeRow0 ->
+                            let
+                                columns =
+                                    Maybe.withDefault "" maybeRow0
+                                        |> String.split ","
+                            in
+                            Stream.forEach
+                                (\row ->
+                                    if String.isEmpty row then
+                                        IO.none
+
+                                    else
+                                        let
+                                            values =
+                                                String.split "," row
+                                        in
+                                        List.map Encode.string values
+                                            |> List.map2 Tuple.pair columns
+                                            |> Encode.object
+                                            |> Encode.encode 2
+                                            |> IO.printLn
+                                )
+                                rowStream
+                        )
             )
-
-
